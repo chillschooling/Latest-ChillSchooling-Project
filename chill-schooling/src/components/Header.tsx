50509e2f79fdfee69ed3095b168365185904d6b4
chillschooling <chillschooling@gmail.com>
2025-09-04 04:35:45 +0530

    five

--- chill-schooling/src/components/Header.tsx
+++ chill-schooling/src/components/Header.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -31,6 +32,35 @@ const Header: React.FC = () => {
         </div>
       </nav>
     </header>
+=======
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is imported
+
+const Header: React.FC = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Chill Schooling</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/resources">Resources</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+>>>>>>> Stashed changes
   );
 };
 
