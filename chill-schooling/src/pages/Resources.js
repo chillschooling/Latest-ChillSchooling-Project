6382e7e4b2d6f89e1dfdf6c7f9921c15745f84aa
chillschooling <chillschooling@gmail.com>
2025-09-04 03:54:17 +0530

    two

---
+++ chill-schooling/src/pages/Resources.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Resources() {
+  return (
+    <div>
+      <h1>Welcome to the Resources Page!</h1>
+      <p>This is where you'll find all the learning resources.</p>
+    </div>
+  );
+}
+
+export default Resources;
