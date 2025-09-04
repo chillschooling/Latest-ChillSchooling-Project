7f078dc9b2703b4ff1062924e0d17f3d31c90d5c
chillschooling <chillschooling@gmail.com>
2025-09-05 02:55:23 +0530

    ten

---
+++ chill-schooling/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
